d80e712e1bfa21230f442e3bf6596881e6a45b65
pochoclo737-a321 <pochoclo737@gmail.com>
2025-09-24 18:02:28 +0200

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.yourcompany.cargo",
+  appName: "Cargo Distributor",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
